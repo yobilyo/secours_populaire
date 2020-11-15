34d308a341d8e66f40aabc1a5ce239508f746033
“yobilyo” <bbb.yousfi@gmail.com>
2020-11-15 15:11:35 +0100

    update .sql file

--- sql/secours.sql
+++ sql/secours.sql
@@ -1,4 +1,4 @@
-DM : maison en groupe 
+/*DM : maison en groupe 
 
 Gestion des projets humanitaires du secours populaire 
 
@@ -7,10 +7,7 @@ humanitaires dans le monde. Pour cela, il souhaite disposer d'un outil lui perme
 
 USER : sinscrire, voir les projets et faire des dons, commenter les projets  
 ADMIN : gérer les users, les projets, les dons, les commentaires. 
-		avec ajout, suppresion, consulter, mise à jour des quatre tables. 
-
-					
-La base de données est la suivante : 
+		avec ajout, suppresion, consulter, mise à jour des quatre tables. */
 
 drop database if exists secours ; 
 create database secours; 
@@ -23,12 +20,18 @@ create table membre (
 	adresse varchar(50),
 	tel varchar(20),
 	email varchar(100), 
-	mdp varchar(255), 
+	mdp varchar(255),
 	primary key(idmembre)
 );
 
-insert into membre values (null, "ASSUNCAO","Hugo", "15 rue jean moulin - Paris", "0633825296", "hugo@3dsoft.fr", "A4C4CBGFDA"),
-						(null, "BEN AMHED","BEN", "5 rue de clery - Paris", "0755859625", "benamhed@gmail.fr", "484DE484");
+insert into membre values
+	(null, "BARTHEZ", "Fabien", "10, rue de Metz 75010 Paris", "0601152595", "a@gmail.com", "123"),
+	(null, "THURAM", "Lilian", "3, avenue de la Défense 75016 Paris", "0603672856", "b@gmail.com", "456"),
+	(null, "DESCHAMPS", "Didier", "37, boulevard Saint-Germain Défense 69004 Lyon", "0638181580", "c@gmail.com", "789"),
+	(null, "VIERA", "Patrick", "37, avenue des Festivals 06400 Cannes", "0612036794", "d@gmail.com", "101112"),
+	(null, "ZINEDINE", "Zidane", "43, rue de Nice 06105 Nice", "0638651525", "e@gmail.com", "131415"),
+	(null, "PLATINI", "Michel", "37, avenue des Festivals 06400 Cannes", "0685146958", "f@gmail.com", "161718"),
+;
 
 create table projet (
 	idprojet int(3) not null auto_increment, 
@@ -41,8 +44,13 @@ create table projet (
 	primary key(idprojet)
 );
 
-INSERT INTO projet VALUES (NULL, "lutte contre la faim", "2020-11-12", "Afrique du Sud", "Cape Town", 15000.50, 2307.89),
-(NULL, "fournir des vetements pour l'hiver", "2020-11-13", "Senegal", "Dakar", 3500.68, 666.66);
+INSERT INTO projet VALUES
+	(NULL, "Lutte contre la faim", "2020-11-12", "Afrique du Sud", "Cape Town", 75000, 0),
+	(NULL, "Fournir des vetements pour l'hiver", "2020-09-16", "Senegal", "Dakar", 3530.68, 0)
+	(NULL, "Aide à l'enfance", "2020-09-05", "Pérou", "Lima", 2700, 0),
+	(NULL, "Construction d'écoles", "2020-10-08", "Cambodge", "Phnom Penh", 11380, 0),
+	(NULL, "Activités sportives et culturelles", "2020-10-19", "Madagascar", "Tananarive", 9230, 0),
+;
 
 create table don (
 	iddon int(3) not null auto_increment, 
@@ -56,7 +64,15 @@ create table don (
 	foreign key(idmembre) references membre(idmembre)
 );
 
-insert into don values (null, "2020-11-01", 4000, "Merci",1,2), (null, "2020-12-01", 5000, "Bravo", 1,1);
+insert into don values
+	(null, "2020-12-01", 375, "Merci", 1, 1),
+	(null, "2020-12-03", 220, "Bravo", 1, 2),
+	(null, "2020-12-02", 135, "Bon courage!", 2, 3),
+	(null, "2020-12-04", 400, "Excellent travail", 3, 5),
+	(null, "2020-12-04", 1230, "Super", 4, 2),
+	(null, "2020-12-04", 525, "Super", 4, 3),
+	(null, "2020-12-04", 888, "Merci de vos efforts", 5, 5)
+;
 
 create table commentaire (
 	idcomment int(3) not null auto_increment, 
@@ -70,10 +86,12 @@ create table commentaire (
 	foreign key(idmembre) references membre(idmembre)
 );
 
-insert into commentaire values (null, "2020-12-02", "Je suis satisfait", 18, 1,1), (null, "2020-10-5", "Tres content",16, 1,2);
-
-
-
+insert into commentaire values
+	(null, "2020-12-02", "Ils font preuve d'humanité, ils sont très solidaires et ils nous donnent leur temps pour nous servir. MERCI BEAUCOUP!", 18, 1, 1),
+	(null, "2020-12-05", "Leur approche si conviviale auprès des gens démunis. Merci", 14, 1, 2)
+	(null, "2020-12-05", "La première fois que j'ai Croisé les Enfants en vacances à la plage tout Heureux. Grâce au Secours Populaire. Bravo", 16, 3, 5)
+	(null, "2020-12-05", "Une organisation indispensable dans notre monde où les catastrophes individuelles et collectives sont hélas très nombreuses..", 13, 4, 3)
+;
 
 create view don_membre_projet as (
     select d.iddon, d.datedon, d.somme, d.appreciation, p.description, m.nom
@@ -81,7 +99,6 @@ create view don_membre_projet as (
     where d.idprojet = p.idprojet
     and d.idmembre = m.idmembre
 );
-
 
 create view commentaire_membre_projet as (
     select c.idcomment, c.datecomment, c.contenu, c.note, p.description, m.nom
