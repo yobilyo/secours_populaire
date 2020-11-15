81e59b91c30cd30d1d15316e1b1ccb1f60ba2b1b
“yobilyo” <bbb.yousfi@gmail.com>
2020-11-15 17:17:50 +0100

    fix sql triggers to the top

--- sql/secours.sql
+++ sql/secours.sql
@@ -25,15 +25,6 @@ create table membre (
 	primary key(idmembre)
 );
 
-insert into membre values
-	(null, "BARTHEZ", "Fabien", "10, rue de Metz 75010 Paris", "0601152595", "a@gmail.com", "123", "admin"),
-	(null, "THURAM", "Lilian", "3, avenue de la Défense 75016 Paris", "0603672856", "b@gmail.com", "456", "user"),
-	(null, "DESCHAMPS", "Didier", "37, boulevard Saint-Germain Défense 69004 Lyon", "0638181580", "c@gmail.com", "789", "user"),
-	(null, "VIERA", "Patrick", "37, avenue des Festivals 06400 Cannes", "0612036794", "d@gmail.com", "101112", "user"),
-	(null, "ZINEDINE", "Zidane", "43, rue de Nice 06105 Nice", "0638651525", "e@gmail.com", "131415", "user"),
-	(null, "PLATINI", "Michel", "37, avenue des Festivals 06400 Cannes", "0685146958", "f@gmail.com", "161718", "admin")
-;
-
 create table projet (
 	idprojet int(3) not null auto_increment, 
 	description varchar(50),
@@ -44,14 +35,6 @@ create table projet (
 	sommecollectee float, 
 	primary key(idprojet)
 );
-
-INSERT INTO projet VALUES
-	(NULL, "Lutte contre la faim", "2020-11-12", "Afrique du Sud", "Cape Town", 75000, 0),
-	(NULL, "Fournir des vetements pour l'hiver", "2020-09-16", "Senegal", "Dakar", 3530.68, 0),
-	(NULL, "Aide a l'enfance", "2020-09-05", "Pérou", "Lima", 2700, 0),
-	(NULL, "Construction d'écoles", "2020-10-08", "Cambodge", "Phnom Penh", 11380, 0),
-	(NULL, "Activités sportives et culturelles", "2020-10-19", "Madagascar", "Tananarive", 9230, 0)
-;
 
 create table don (
 	iddon int(3) not null auto_increment, 
@@ -65,16 +48,6 @@ create table don (
 	foreign key(idmembre) references membre(idmembre)
 );
 
-insert into don values
-	(null, "2020-12-01", 375, "Merci", 1, 1),
-	(null, "2020-12-03", 220, "Bravo", 1, 2),
-	(null, "2020-12-02", 135, "Bon courage!", 2, 3),
-	(null, "2020-12-04", 400, "Excellent travail", 3, 5),
-	(null, "2020-12-04", 1230, "Super", 4, 2),
-	(null, "2020-12-04", 525, "Super", 4, 3),
-	(null, "2020-12-04", 888, "Merci de vos efforts", 5, 5)
-;
-
 create table commentaire (
 	idcomment int(3) not null auto_increment, 
 	datecomment date,
@@ -86,13 +59,6 @@ create table commentaire (
 	foreign key(idprojet) references projet(idprojet), 
 	foreign key(idmembre) references membre(idmembre)
 );
-
-insert into commentaire values
-	(null, "2020-12-02", "Ils font preuve d'humanité, ils sont très solidaires et ils nous donnent leur temps pour nous servir. MERCI BEAUCOUP!", 18, 1, 1),
-	(null, "2020-12-05", "Leur approche si conviviale auprès des gens démunis. Merci", 14, 1, 2),
-	(null, "2020-12-05", "La première fois que j'ai Croisé les Enfants en vacances à la plage tout Heureux. Grâce au Secours Populaire. Bravo", 16, 3, 5),
-	(null, "2020-12-05", "Une organisation indispensable dans notre monde où les catastrophes individuelles et collectives sont hélas très nombreuses..", 13, 4, 3)
-;
 
 create view don_membre_projet as (
     select d.iddon, d.datedon, d.somme, d.appreciation, p.description, m.nom
@@ -143,3 +109,37 @@ BEGIN
 	WHERE old.idprojet = idprojet;
 END $
 DELIMITER ;
+
+insert into membre values
+	(null, "BARTHEZ", "Fabien", "10, rue de Metz 75010 Paris", "0601152595", "a@gmail.com", "123", "admin"),
+	(null, "THURAM", "Lilian", "3, avenue de la Défense 75016 Paris", "0603672856", "b@gmail.com", "456", "user"),
+	(null, "DESCHAMPS", "Didier", "37, boulevard Saint-Germain Défense 69004 Lyon", "0638181580", "c@gmail.com", "789", "user"),
+	(null, "VIERA", "Patrick", "37, avenue des Festivals 06400 Cannes", "0612036794", "d@gmail.com", "101112", "user"),
+	(null, "ZINEDINE", "Zidane", "43, rue de Nice 06105 Nice", "0638651525", "e@gmail.com", "131415", "user"),
+	(null, "PLATINI", "Michel", "37, avenue des Festivals 06400 Cannes", "0685146958", "f@gmail.com", "161718", "admin")
+;
+
+INSERT INTO projet VALUES
+	(NULL, "Lutte contre la faim", "2020-11-12", "Afrique du Sud", "Cape Town", 75000, 0),
+	(NULL, "Fournir des vetements pour l'hiver", "2020-09-16", "Senegal", "Dakar", 3530.68, 0),
+	(NULL, "Aide a l'enfance", "2020-09-05", "Pérou", "Lima", 2700, 0),
+	(NULL, "Construction d'écoles", "2020-10-08", "Cambodge", "Phnom Penh", 11380, 0),
+	(NULL, "Activités sportives et culturelles", "2020-10-19", "Madagascar", "Tananarive", 9230, 0)
+;
+
+insert into don values
+	(null, "2020-12-01", 375, "Merci", 1, 1),
+	(null, "2020-12-03", 220, "Bravo", 1, 2),
+	(null, "2020-12-02", 135, "Bon courage!", 2, 3),
+	(null, "2020-12-04", 400, "Excellent travail", 3, 5),
+	(null, "2020-12-04", 1230, "Super", 4, 2),
+	(null, "2020-12-04", 525, "Super", 4, 3),
+	(null, "2020-12-04", 888, "Merci de vos efforts", 5, 5)
+;
+
+insert into commentaire values
+	(null, "2020-12-02", "Ils font preuve d'humanité, ils sont très solidaires et ils nous donnent leur temps pour nous servir. MERCI BEAUCOUP!", 18, 1, 1),
+	(null, "2020-12-05", "Leur approche si conviviale auprès des gens démunis. Merci", 14, 1, 2),
+	(null, "2020-12-05", "La première fois que j'ai Croisé les Enfants en vacances à la plage tout Heureux. Grâce au Secours Populaire. Bravo", 16, 3, 5),
+	(null, "2020-12-05", "Une organisation indispensable dans notre monde où les catastrophes individuelles et collectives sont hélas très nombreuses..", 13, 4, 3)
+;
